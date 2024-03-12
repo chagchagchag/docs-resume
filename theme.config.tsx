574690c868e91cbfd2734816a0372d2f555955d5
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-12 23:05:15 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Resume</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-resume',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-resume',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
